dfa39b0d1436e35911e9cd14a4a15a0586048472
Koichi Shiraishi <zchee.io@gmail.com>
2022-08-08 13:34:45 +0900

    proxy: add Config
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- main.go
+++ main.go
@@ -13,16 +13,18 @@ import (
 )
 
 func main() {
-	var flags flag.FlagSet
+	cfg := &proxy.Config{}
 
-	protogen.Options{
+	flags := flag.NewFlagSet("protoc-gen-proxy", flag.ExitOnError)
+	flags.BoolVar(&cfg.Standalone, "standalone", false, "standalone mode")
+
+	opts := protogen.Options{
 		ParamFunc: flags.Set,
-	}.Run(func(gen *protogen.Plugin) error {
-		gen.SupportedFeatures = proxy.SupportedFeatures
-
-		for _, f := range gen.Files {
+	}
+	opts.Run(func(p *protogen.Plugin) error {
+		for _, f := range p.Files {
 			if f.Generate {
-				proxy.GenerateFile(gen, f)
+				proxy.GenerateFile(p, f, cfg)
 			}
 		}
 
