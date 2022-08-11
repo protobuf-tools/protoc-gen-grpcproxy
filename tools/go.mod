0698d55b5bf52943b28c33b6b72456721894e8f1
Koichi Shiraishi <zchee.io@gmail.com>
2022-08-11 20:21:37 +0900

    tools: add tools.go
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ tools/go.mod
@@ -0,0 +1,8 @@
+module github.com/protobuf-tools/protoc-gen-proxy/tools
+
+go 1.19
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.1-0.20220422173813-e8d06c51a523
+	google.golang.org/protobuf v1.28.1
+)
---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+// Package tools manages tools using during development.
+
+//go:build tools
+
+package tools
+
+import (
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
