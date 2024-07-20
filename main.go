e698185102f477363591ab08a8f856036151b018
Koichi Shiraishi <zchee.io@gmail.com>
2024-07-20 13:29:28 +0900

    main: support editions feature
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"flag"
 
 	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 
 	"github.com/protobuf-tools/protoc-gen-grpcproxy/proxy"
 )
@@ -21,12 +24,18 @@ func main() {
 	opts := protogen.Options{
 		ParamFunc: flags.Set,
 	}
-	pluginFn := func(p *protogen.Plugin) error {
-		for _, f := range p.Files {
+	pluginFn := func(gen *protogen.Plugin) error {
+		for _, f := range gen.Files {
+			if f.Desc.Syntax() != protoreflect.Proto3 {
+				continue
+			}
 			if f.Generate {
-				proxy.GenerateFile(p, f, cfg)
+				proxy.GenerateFile(gen, f, cfg)
 			}
 		}
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(pluginpb.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS)
+		gen.SupportedEditionsMinimum = descriptorpb.Edition_EDITION_PROTO2
+		gen.SupportedEditionsMaximum = descriptorpb.Edition_EDITION_2023
 
 		return nil
 	}
